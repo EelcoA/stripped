8589b6214e99c16544277208af445baae97435cc
Eelco Aartsen <eelco@aesset.nl>
2021-01-07 13:28:01 +0100

    Get SECRET_KEY from environment, with a default, testing if mamage.py collectstatic still works

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ ENVIRONMENT = 'stripped'
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'TaTiNAtHoLmINGECtOwDOnEuRucentITisHIaNtIOreSTrAgEO'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='JustADefaultSecretKey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEBUG'
