78fb2dc419d2cbc98e7dd3a79691a2a157b96e8f
Eelco Aartsen <eelco@aesset.nl>
2021-01-24 17:28:42 +0100

    Preparing release 1.1.1

--- util.py
+++ util.py
@@ -1,6 +1,6 @@
 import markdown
 
-def convert_changelog_to_html():
+def convert_changelog_to_html(data):
     with open('CHANGELOG.md', 'r') as f:
         text = f.read()
         html = markdown.markdown(text)
