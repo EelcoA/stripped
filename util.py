1706513d27b3bbb3806f44a5afe28666cb5e43ce
Eelco Aartsen <eelco@aesset.nl>
2021-01-24 17:23:48 +0100

    commit to test prerelease extension point

---
+++ util.py
@@ -0,0 +1,9 @@
+import markdown
+
+def convert_changelog_to_html():
+    with open('CHANGELOG.md', 'r') as f:
+        text = f.read()
+        html = markdown.markdown(text)
+
+    with open('templates/changelog.html', 'w') as f:
+        f.write(html)
