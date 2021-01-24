bbc2b177d34aa0129f9ff7629d8d19ca0363dcd1
Eelco Aartsen <eelco@aesset.nl>
2021-01-24 17:43:12 +0100

    Preparing release 1.1.1

--- util.py
+++ util.py
@@ -5,5 +5,14 @@ def convert_changelog_to_html(data):
         text = f.read()
         html = markdown.markdown(text)
 
+
     with open('templates/changelog.html', 'w') as f:
+        f.write("{ % extends '_base.html' %}")
+        f.write("{ % load static %}")
+        f.write("{ % block title %}NPO-RM Changelog { % endblock title %}")
+        f.write("{ % block content %}")
+        f.write("<div class='container-fluid pl-3'>")
+        f.write("<br>")
         f.write(html)
+        f.write("</div>")
+        f.write("{% endblock content %}")
